0048efabcef464374d6e0ee768567dd8f45e633c
Roman Browin <roman.browin@yandex.ru>
2021-11-05 21:51:56 +0400

    add basket functions

---
+++ semester/semester/helpers.py
@@ -0,0 +1,45 @@
+from flask import session, request
+
+
+def create_basket():
+    session['basket'] = []
+    session['total'] = 0
+
+
+def basket_update():
+    if not session.get('basket'):
+        create_basket()
+
+        session['basket'] += [{
+            'product_id': request.form.get('id'),
+            'product_name': request.form.get('name'),
+            'product_image': request.form.get('image'),
+            'product_price': int(request.form.get('price')),
+            'product_quantity': 1
+        }]
+        print("))")
+    else:
+        for s in session['basket']:
+            print(s)
+            if s['product_id'] == request.form.get('id'):
+                s['product_quantity'] += 1
+                s['product_price'] += int(request.form.get('price'))
+                session['total'] += int(request.form.get('price'))
+                return str(len(session['basket']))
+
+        session['basket'] += [{
+            'product_id': request.form.get('id'),
+            'product_name': request.form.get('name'),
+            'product_image': request.form.get('image'),
+            'product_price': int(request.form.get('price')),
+            'product_quantity': 1
+            }]
+
+    session['total'] += int(request.form.get('price'))
+    return str(len(session['basket']))
+
+
+def basket_clean():
+    session.pop('basket', None)
+    session.pop('total', None)
+    return 'Visits deleted'
