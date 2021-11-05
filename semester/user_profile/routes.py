fe988076ef36672041050c6c8ca32bfa9341bc41
Roman Browin <roman.browin@yandex.ru>
2021-11-05 21:27:24 +0400

    create edit page with route

--- semester/user_profile/routes.py
+++ semester/user_profile/routes.py
@@ -30,3 +30,49 @@ def profile():
         ))
 
 
+@user_profile.route('/edit', methods=['POST', 'GET'])
+@login_required
+def edit():
+    form = EditForm()
+    user = db.session.query(User).filter(User.id == current_user.get_id()).first()
+    if request.method == 'GET':
+        return render_template(
+            'user_profile/edit.html',
+            menu_url=MainMenu.query.all(),
+            user=user,
+            form=form
+        )
+    elif request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        number = request.form.get('number')
+        address = request.form.get('address')
+        if not name or not number or not address or not email:
+            flash('Не все поля заполнены!', category='unfilled_error')
+        elif '@' not in email or '.' not in email:
+            flash('Некорректный email!', category='validation_error')
+        elif any(map(str.isdigit, name)) is True:
+            flash('Некорректное имя!', category='validation_error')
+        elif any(map(str.isdigit, number)) is False:
+            flash('Некорректный номер!', category='validation_error')
+        else:
+            user.name = name
+            user.phone_number = number
+            user.address = address
+            db.session.commit()
+            flash("Изменения успешно применены!", category='success')
+            return redirect(url_for(
+                'user_profile.profile',
+                menu_url=MainMenu.query.all(),
+                user=user
+            ))
+        print(request)
+        print(get_flashed_messages(True))
+        return render_template(
+            'user_profile/edit.html',
+            menu_url=MainMenu.query.all(),
+            user=user,
+            form=form
+        )
+    else:
+        raise Exception(f'Method {request.method} not allowed')
